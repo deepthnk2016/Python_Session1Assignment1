fbcbd471635de0857384673b1021ef3bbbe14185
Deepak Ray <deepthnk2016@gmail.com>
2018-08-05 00:43:10 +0530

    Add files via upload

---
+++ Session1Assignment1.py
@@ -0,0 +1,6 @@
+#-----------------------------------------------------------------
+# Python - Session 1 Assignment 1
+# Descr - First Sample program in python
+#-----------------------------------------------------------------
+name=input('Enter Your Name:')
+print('You have entered %s' %(name))
